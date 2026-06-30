b12ea97b9a83d353c3b148006e6dc297c6efa7fd
lakshmipriyaks31-art <lakshmipriya.ks.31@gmail.com>
2026-06-30 16:49:36 +0530

    cookies  strict

--- server/app/controller/user.controller.js
+++ server/app/controller/user.controller.js
@@ -8,13 +8,13 @@ exports.getUser = async(req,res) => {
     res.cookie("accessToken", accesstoken, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 15 * 60 * 1000,
     })
     res.cookie("refreshToken", refreshtoken, {
         httpOnly: true,
-        secure: false, // true in production
-        sameSite: "strict",
+        secure: true, // true in production
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
     ApiResponse.success(res, currentuser,Message_OK, OK);
@@ -25,14 +25,16 @@ exports.Register = async(req,res) => {
     const {accesstoken,refreshtoken,currentuser} =await Register(req.body)
     res.cookie("accessToken", accesstoken, {
         httpOnly: true,
+        domain: "https://ai-chat-assistant-kslp.vercel.app/",
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
     maxAge: 15 * 60 * 1000,
     })
     res.cookie("refreshToken", refreshtoken, {
         httpOnly: true,
         secure: false, // true in production
-        sameSite: "strict",
+        domain: "https://ai-chat-assistant-kslp.vercel.app/",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
     ApiResponse.success(res, currentuser,Message_CREATED, CREATED);
@@ -43,13 +45,15 @@ exports.Login = async(req,res) => {
     res.cookie("accessToken", accesstoken, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
+        domain: "https://ai-chat-assistant-kslp.vercel.app/",
         maxAge: 15 * 60 * 1000,
     })
     res.cookie("refreshToken", refreshtoken, {
         httpOnly: true,
         secure: false, // true in production
-        sameSite: "strict",
+        sameSite: "none",
+        domain: "https://ai-chat-assistant-kslp.vercel.app/",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
     ApiResponse.success(res, currentuser,Message_OK, OK);
@@ -61,12 +65,14 @@ exports.logout = async(req,res) => {
     res.clearCookie("accessToken", {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
+        domain: "https://ai-chat-assistant-kslp.vercel.app/",
     })
     res.clearCookie("refreshToken", {
         httpOnly: true,
         secure: false, // true in production
-        sameSite: "strict"
+        sameSite: "none",
+        domain: "https://ai-chat-assistant-kslp.vercel.app/",
     });
     ApiResponse.success(res,Message_Logout, OK);
 
